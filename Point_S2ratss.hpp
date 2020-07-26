cc00b4c7e24205e336b264043e915c5c1adb7b84
Martin Seybold <martin.seybold@gmx.de>
2020-07-26 21:11:20 +1000

    orientV sphere

--- Point_S2ratss.hpp
+++ Point_S2ratss.hpp
@@ -34,14 +34,14 @@ using Direction_3 = CGAL::Direction_3<Kernel>;
 class PointS2ratss {
     private:
         Point_3 cgal_point;
-        
         inline static ratss::ProjectS2   projection;
         inline static int                projectionPrecision = 15;
-        // inline static traitsXY           projectionXY;
 
+        // inline static traitsXY           projectionXY;
         // inline static OrientationXY      orientationXY;
 
-        inline static int orientV(Point_3& self, Point_3& other );
+        inline static int       orientV( Point_3& self, Point_3& other );
+        inline static Point_3   intersection( Point_3& s1, Point_3& t1, Point_3& s2, Point_3& t2 );
 
     public:
         PointS2ratss(double lon, double lat) {
@@ -66,7 +66,7 @@ class PointS2ratss {
                     || -(s.z()) != t.z() ;
         }
 
-        static int v_orientation(PointS2ratss& self, PointS2ratss& other){
+        inline static int orientV(PointS2ratss& self, PointS2ratss& other){
             return orientV( self.cgal_point, other.cgal_point );
         }
 
@@ -75,13 +75,13 @@ class PointS2ratss {
         // -1 negative
         // 1 positive
         // 0 on the line
-        static int line_orientation(
+        static int orientE(
             PointS2ratss source, PointS2ratss target, PointS2ratss p);
 
         // Detmines the oriented side of the vertical
         // line through the intersection of line(s1, t2)
         // and line line(s2, t2) which contains p
-        static int intersection_v_orientation(
+        static int orientV(
             PointS2ratss s_1, PointS2ratss t_1,
             PointS2ratss s_2, PointS2ratss t_2,
             PointS2ratss p
@@ -89,15 +89,11 @@ class PointS2ratss {
         
         // Compares the intersection of l(s, t) and l(s1, t1)
         // with the intersection of l(s, t) and l(s2, t2)
-        static int intersection_orientation(
+        static int orientV(
             PointS2ratss s, PointS2ratss t,
             PointS2ratss s_1, PointS2ratss t_1,
             PointS2ratss s_2, PointS2ratss t_2
         );
-
-        // lazy x() { return cgal_point.x(); }
-        // lazy y() { return cgal_point.y(); }        
-        
 };
 
 
@@ -108,46 +104,58 @@ class PointS2ratss {
 // Predicate for vert orientation on the cylindrical surface (left=-1; on=0; right=+1)
 // Returns `left' iff `other' is left of the vertical half great circle through North,point,South
 int PointS2ratss::orientV( Point_3& self, Point_3& other) {
-    //TODO: predicate signatures should be call-by-reference
+    // This ASSUMES all points having length==1 !!!
+    if( self.x() == other.x() &&
+        self.y() == other.y() &&
+        self.z() == other.z()   )
+        return 0;
 
-    // Algo:
-    // * IF point == other THEN  return 0;
-    // * Drop the z coodrinate and consider the 2D orientation(pXY, ORIGIN, oXY)
-    // * CASE: pXY is WEST    CASE pXY EAST is symmetric
-    //      * IF  orient == RIGHT_TURN               THEN    return +1;
-    //      * EIF orient == LEFT_TURN                THEN    return (oXY WEST)? +1 : -1;
-    //      * EIF orient == COLLINEAR && oXY EAST    THEN    return +1
-    //      * EIF orient == COLLINEAR                THEN    return (p.z)? +1 : -1
-    
+    PointXY selfXY(   self.x(), self.y()  ),
+            otherXY( other.x(), other.y() ),
+            myOrigin = CGAL::ORIGIN;    
 
-    PointXY selfXY( self.x(), self.y() );
-    PointXY otherXY( other.x(), other.y() );    
-    
+    //     +\- |+ o 
+    //       s | /
+    //        \|/    
+    // --------+-------> x  
+    //         '\
+    // West    ' \    East
+    // Both are half-closed surfaces
+    bool   isSelfWest =  self.x() < 0 || (  self.x()==0 &&  self.y() < 0),
+         isOtherfWest = other.x() < 0 || ( other.x()==0 && other.y() < 0);      // TODO: simplify 
 
-
-
-    // switch ( orientationXY( pXY, CGAL::ORIGIN, oXY ) ){
-    //     case CGAL::COLLINEAR: 
-    //         std::cout << "are collinear\n";
-    //         break;
-    //     case CGAL::LEFT_TURN:
-    //         std::cout << "make a left turn\n";
-    //         break;
-    //     case CGAL::RIGHT_TURN: 
-    //         std::cout << "make a right turn\n";
-    //         break;
-    // }
-    
-    // Both, East and West, are half-closed surfaces
-    // bool pEast = p.x < 0 || ( p.x==0 && p.y < 0);
-    // bool oEast = o.x < 0 || ( o.x==0 && o.y < 0); 
-
-    return 0;
+    if( isSelfWest ){
+        switch ( CGAL::orientation( selfXY, myOrigin, otherXY ) ){
+            case CGAL::RIGHT_TURN: 
+                return +1;
+            case CGAL::LEFT_TURN:
+                return  isOtherfWest ? -1 : +1;
+            case CGAL::COLLINEAR: 
+                if( !isOtherfWest ){
+                    return +1;
+                } else{
+                    return (other.z() > self.z()) ? -1 : +1;
+                }
+        }
+    } else {
+        switch ( CGAL::orientation( selfXY, myOrigin, otherXY ) ){
+            case CGAL::LEFT_TURN: 
+                return -1;
+            case CGAL::RIGHT_TURN:
+                return  isOtherfWest ? -1 : +1;
+            case CGAL::COLLINEAR: 
+                if( isOtherfWest ){
+                    return -1;
+                } else{
+                    return (self.z() > other.z()) ? +1 : -1;
+                }
+        }
+    }
 }
 
 // Assumes:     source left-of target  AND  isValidSeg( source, target )
 // Returns -1/0/+1 for below/on/above
-int PointS2ratss::line_orientation(
+int PointS2ratss::orientE(
     PointS2ratss source, PointS2ratss target, PointS2ratss p) {
         Point_3& src = source.cgal_point,
                  trg = target.cgal_point,
@@ -160,82 +168,46 @@ int PointS2ratss::line_orientation(
 
 
 // ASSUMES:  Arc(s1,t1) and Arc(s2,t2) are crossing
-int PointS2ratss::intersection_v_orientation(
+int PointS2ratss::orientV(
             PointS2ratss s_1, PointS2ratss t_1,
             PointS2ratss s_2, PointS2ratss t_2,
             PointS2ratss p
         ) {
-            Point_3& _s1 = s_1.cgal_point, _t1 = t_1.cgal_point,
-                     _s2 = s_2.cgal_point, _t2 = t_2.cgal_point,
-                     _p  = p.cgal_point;
-                     
-            Plane_3 seg1 (CGAL::ORIGIN, _s1,_t1),
-                    seg2 (CGAL::ORIGIN, _s2,_t2);
-            CGAL::cpp11::result_of<Intersect_3(Plane_3, Plane_3)>::type
-                result = intersection(seg1, seg2);
-            if (result) {
-                if (const Line_3* line = boost::get<Line_3>(&*result)) {
-                    Direction_3 posDir = line->direction();
-                    Point_3 i ( posDir.dx(),  posDir.dy(),  posDir.dz() );
-
-                    assert( orientV(_s1, i) == +1 );
-                    assert( orientV(_t1, i) == -1 );
-                    assert( orientV(_s2, i) == +1 );
-                    assert( orientV(_t2, i) == -1 );
-
-                    return orientV(i, _p);
-                } else {
-                    // TODO: exception segs are non-crossing!
-                }
-            }
-    
+    Point_3 i = intersection(   s_1.cgal_point, t_1.cgal_point,
+                                s_2.cgal_point, t_2.cgal_point  );
+    return orientV( i, p.cgal_point );    
 }
 
-int PointS2ratss::intersection_orientation(
+int PointS2ratss::orientV(
             PointS2ratss s,     PointS2ratss t,
             PointS2ratss s_1,   PointS2ratss t_1,
             PointS2ratss s_2,   PointS2ratss t_2
         ) {
+    Point_3 i1 = intersection(   s.cgal_point, t.cgal_point,
+                                 s_1.cgal_point, t_1.cgal_point  );
+    Point_3 i2 = intersection(   s.cgal_point, t.cgal_point,
+                                 s_2.cgal_point, t_2.cgal_point  );
+    return orientV(i1, i2);
+}
 
-    // Line_2 line = Line_2(s.get_cgal_point(), t.get_cgal_point());
-    // Line_2 line_1 = Line_2(s_1.get_cgal_point(), t_1.get_cgal_point());
-    // Line_2 line_2 = Line_2(s_2.get_cgal_point(), t_2.get_cgal_point());
+Point_3 PointS2ratss::intersection( Point_3& s1, Point_3& t1, Point_3& s2, Point_3& t2 ){
+    Plane_3 seg1 (CGAL::ORIGIN, s1, t1),
+            seg2 (CGAL::ORIGIN, s2, t2);
+    CGAL::cpp11::result_of<Intersect_3(Plane_3, Plane_3)>::type
+        result = CGAL::intersection(seg1, seg2);
+    if (result) {
+        if (const Line_3* line = boost::get<Line_3>(&*result)) {
+            Direction_3 posDir = line->direction();
+            Point_3 i ( posDir.dx(),  posDir.dy(),  posDir.dz() );
 
-    // auto intersection_1 = CGAL::intersection(line, line_1);
-    // Point_2 ip_1;
+            assert( orientV(s1, i) == +1 );
+            assert( orientV(t1, i) == -1 );
+            assert( orientV(s2, i) == +1 );
+            assert( orientV(t2, i) == -1 );
 
-    // if (intersection_1) {
-    //     ip_1 = boost::get<Point_2>(*intersection_1);
-    // }   else {
-    //     throw std::logic_error("lines do not intersect");
-    // }
-
-    // auto intersection_2 = CGAL::intersection(line, line_2);
-    // Point_2 ip_2;
-
-    // if (intersection_2) {
-    //     ip_2 = boost::get<Point_2>(*intersection_2);
-    // } else {
-    //     throw std::logic_error("lines do not intersect");
-    // }
-
-    // if (ip_2.x() < ip_1.x())
-    //     return 1;
-    // if (ip_2.x() > ip_1.x())
-    //     return -1;
-
-    // Line_2 v_line = Line_2(shear(ip_1), Vector_2(0, 1));
-
-    // auto side = v_line.oriented_side(shear(ip_2));
-
-    // switch(side) {
-    //     case CGAL::ON_NEGATIVE_SIDE:
-    //         return -1;
-    //     case CGAL::ON_POSITIVE_SIDE:
-    //         return 1;
-    //     case CGAL::ON_ORIENTED_BOUNDARY:
-    //         return 0;
-    // }
-
-    // throw std::logic_error("failed to compare intersection points");
+            return i;
+        } else {
+            // TODO: exception segs are non-crossing!
+        }
+    }
 }
