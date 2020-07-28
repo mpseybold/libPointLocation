e626520f42c8661b08f23f35d19a18c08b1920e1
milutinB <milutin.brankovic3@gmail.com>
2020-07-28 18:53:16 +1000

    working on recursive algorithms

--- TSD_algorithms/merge.cpp
+++ TSD_algorithms/merge.cpp
@@ -2,6 +2,5 @@
 
 template <class PointType, class OrderType>
 Node<PointType, OrderType>* TSD<PointType, OrderType>::merge(Node<PointType, OrderType>* below, Node<PointType, OrderType>* above) {
-    //TODO: Implement this function.
-    
+    return nullptr;
 }
