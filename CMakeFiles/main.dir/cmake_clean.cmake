5e332c3f9af76f0e68b57f163e5276e805877356
milutinB <milutin.brankovic3@gmail.com>
2020-08-14 15:06:23 +1000

    lastest update

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/BoundingTrap.cpp.o"
+  "CMakeFiles/main.dir/Cut.cpp.o"
+  "CMakeFiles/main.dir/Point_2D.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/CMakeFiles/Google_Tests_run.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Google_Tests_run.dir/__/BoundingTrap.cpp.o"
+  "CMakeFiles/Google_Tests_run.dir/__/Cut.cpp.o"
+  "CMakeFiles/Google_Tests_run.dir/__/Point_2D.cpp.o"
+  "CMakeFiles/Google_Tests_run.dir/bounding_trap_tests.cpp.o"
+  "CMakeFiles/Google_Tests_run.dir/cut_tests.cpp.o"
+  "CMakeFiles/Google_Tests_run.dir/geom_tests.cpp.o"
+  "CMakeFiles/Google_Tests_run.dir/tsd_tests.cpp.o"
+  "Google_Tests_run"
+  "Google_Tests_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Google_Tests_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
